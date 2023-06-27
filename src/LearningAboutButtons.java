ce6e0f69ed7890c1aa806ff7408a416748b9d185
MiracleAigbogun <miracleaigbogun801@gmail.com>
2023-06-27 11:38:37 -0500

    In this section I learned about JButtons and how you can use them in different ways

---
+++ src/LearningAboutButtons.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class LearningAboutButtons {
+    public static void main(String[] args) {
+        // JButton = a button that preforms an action when clicked
+
+        new MyFrame();
+
+
+
+
+
+    }
+
+}
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,18 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame   {
+
+    JButton button;
     MyFrame(){
+        ImageIcon icon = new ImageIcon("PeakImage.png");
 
-        this.setTitle("Java GUI"); // sets the title of the frame
-        this.setSize(500,500); // sets the size of the frame
-        this.setResizable(false); // stops frame from being resized
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes application when frame is closed
-        this.setVisible(true); // makes the fame visible
 
-        ImageIcon AppIcon = new ImageIcon("AppLogoNew.png"); // creates an icon image
-        this.setIconImage(AppIcon.getImage()); // changes icon of the frame
-        this.getContentPane().setBackground(Color.ORANGE); // changes color of the background
+        button = new JButton();
+        button.setBounds(200, 100, 200, 200);
+        button.addActionListener(e -> System.out.println("foo"));
+        button.setText("I'm a button");
+        button.setFocusable(false);
+        button.setIcon(icon);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500, 500);
+        this.setVisible(true);
+
+        this.add(button);
     }
+
 
 }
